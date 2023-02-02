3cdc53acae4d53885b472006115d07e5e95693ee
LimeBee <kjeanlouis25@gmail.com>
2023-02-02 15:11:41 -0500

    fixed the mongoose error

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
+const MongoStore = require('connect-mongo')
 const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
@@ -12,6 +12,9 @@ const todoRoutes = require('./routes/todos')
 
 
 require('dotenv').config({path: './config/.env'})
+
+mongoose.set('strictQuery', false);
+
 
 // Passport config
 require('./config/passport')(passport)
@@ -29,7 +32,7 @@ app.use(
       secret: 'keyboard cat',
       resave: false,
       saveUninitialized: false,
-      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+      store: MongoStore.create({ mongoUrl: process.env.DB_STRING}),
     })
   )
   
