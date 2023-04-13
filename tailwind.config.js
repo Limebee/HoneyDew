24da1a8ca84adc8295ba5423fb4ecf1b8c76fe68
Karen Jean Louis <67772092+Limebee@users.noreply.github.com>
2023-04-13 10:23:57 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true,
   mode: "jit",
   content: ["./views/**/*.{html,js,ejs}", "./public/**/*.{html,js,ejs}"],
   theme: {
