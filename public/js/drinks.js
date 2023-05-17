42fa8a20663b06a4dfd814e8cc2634432fd45d32
LimeBee <kjeanlouis25@gmail.com>
2023-05-16 23:31:14 -0400

    fixed error

--- public/js/drinks.js
+++ public/js/drinks.js
@@ -3,13 +3,14 @@ const drink = document.querySelector('#search').addEventListener('click', apiReq
 async function apiRequest(){
     const drinkName =  document.querySelector('#drink').value
     try{
-        const response  = await fetch(`https://honeydew-api.onrender.com//api/${drinkName}`)
+        const response  = await fetch(`https://honeydew-api.onrender.com/api/${drinkName}`)
         const data = await response.json()
         console.log(data)
-        document.getElementById('drinkName').innerText = data.name
+        document.getElementById('drinkName').innerHTML = data.name
         document.getElementById('drinkType').innerText = data.drinkType
         document.getElementById('drinkIngredients').innerText = data.ingredients
         document.getElementById('drinkBenefits').innerText = data.benefits
+        document.getElementById('drinkPicture').src = data.src
         
 
     } catch(error) {
